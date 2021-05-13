1525ae255445ba1cb1ab548cfd5c87f1b21288c7
lorenzo amodeo <lrnzmd@gmail.com>
2021-05-13 15:45:54 +0200

    StarGameScreen layout

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,20 +1,51 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, TextInput, Button } from "react-native";
 
-const StartGameScreen = props => {
+const StartGameScreen = (props) => {
   return (
-  <View style={styles.screen}>
-    <Text>the game screen</Text>
-  </View>
-  )
+    <View style={styles.screen}>
+      <Text style={styles.title}>Start new game!!!</Text>
+      <View style={styles.inputContainer}>
+        <Text>Selct a number</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <Button title="Reset" onPress={() => {}} />
+          <Button title="Confirm" onPress={() => {}} />
+        </View>
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    screen:{
-        flex: 1,
-        padding: 10,
-        alignItems: 'center',
-    }
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 20,
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center",
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    elevation: 5, 
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+  },
 });
 
 export default StartGameScreen;
